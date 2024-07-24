b91fcac3e56a0ec1717b1201a6a78fbacbfcd898
amanda-bann <amanda_klap@protonmail.com>
2024-07-23 19:52:18 -0600

    If else statements added for button

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ var cat = document.querySelector(".cat");
 // console.log(cat);
 // console.log(button);
 
-button.addEventListener("click", function(){
-    cat.classList.add("show");
-})
+button.addEventListener("click", function () {
+    if (cat.classList.contains("show")) {
+        // console.log("oui");
+        cat.classList.remove("show");
+        button.innerText = "Attend, reviens!";
+        button.classList.add("disappear");
+    } else {
+        // console.log("non");
+        cat.classList.add("show");
+        button.innerText = "Sortir, chat!";
+        button.classList.remove("disappear");
+    }
+});
